5d3227159b797ece99ccee834d9d26c4f8d7963b
Benjamin <benjaminadji@hotmail.com>
2022-11-05 22:42:54 +0100

    nav system work

--- ProjectFolder/scenes/City/NPCs/NPC.gd
+++ ProjectFolder/scenes/City/NPCs/NPC.gd
@@ -36,7 +36,7 @@ func _on_NPCArea_body_entered(body):
 		print("placeholder for dialogue")
 		#innitiate dialogue
 	elif (body.is_in_group("goodPeople") and type == 0) or (body.is_in_group("badPeople") and type == 1) :
-#		triggered = true
+		triggered = true
 		print("placeholder for fightscene")
 		#innitiate fightscene
 		#Note, with groups it is easy to trigger all enemies in an area since you can trigger a script in all obj in the same group at onceg
