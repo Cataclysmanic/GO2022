8eb5fb4a2f301735fcae8d905a9b0f1b3f7fe769
plexsoup <plexsoup@gmail.com>
2022-11-07 02:30:29 -0800

    first attempt at a paperdoll with pointing

--- ProjectFolder/scenes/Player/SimpleDetectivetForTesting.gd
+++ ProjectFolder/scenes/Player/SimpleDetectivetForTesting.gd
@@ -3,12 +3,16 @@ extends KinematicBody
 
 var speed = 1.5
 var sprint_multiple = 3.0
+var last_movement_vector = Vector3.ZERO # to know when to switch animations.. only on transitions
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
+func _unhandled_input(event):
+	if event.is_action_pressed("flashlight"):
+		toggle_flashlight()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,7 +32,35 @@ func _process(delta):
 	if not Input.is_action_pressed("sprint"):
 		sprint = 1.0
 
-	var _collision = move_and_collide(movement_vector * speed * sprint * delta)
+	var total_movement = movement_vector * speed * sprint
+	var _collision = move_and_collide(total_movement * delta)
+	play_movement_animations(total_movement)
+	last_movement_vector = total_movement
+
+func play_movement_animations(movement_vector:Vector3):
+	var paperDoll = $"3DPaperDoll"
+	paperDoll.point_legs_at(movement_vector)
+	if movement_vector > last_movement_vector:
+		if Input.is_action_pressed("shift"):
+			paperDoll.start_running()
+		else:
+			paperDoll.start_walking()
+	elif movement_vector < last_movement_vector:
+		if movement_vector.length_squared() > 0:
+			paperDoll.start_walking()
+		else:
+			paperDoll.relax()
+
+
+func toggle_flashlight():
+	if $Flashlight.is_visible_in_tree():
+		$Flashlight.hide()
+		$"3DPaperDoll".relax()
+	else:
+		$Flashlight.show()
+		$"3DPaperDoll".point_gun()
+		
+	
 	
 func _physics_process(_delta):
 	
@@ -46,6 +78,9 @@ func point_flashlight_at_mouse():
 	var intersection = space_state.intersect_ray(rayOrigin, rayEnd)
 
 	if not intersection.empty():
-		var pos = intersection.position
-		$Flashlight.look_at(Vector3(pos.x, 0, pos.z), Vector3(0,1,0))
+		var targetPos = intersection.position
+		#$Flashlight.look_at(Vector3(targetPos.x, 0, targetPos.z), Vector3(0,1,0))
+		$Flashlight.look_at(targetPos, Vector3(0,1,0))
+		
 		#mesh.rotate_y(deg2rad(180))
+		$"3DPaperDoll".point_torso_at(Vector3(targetPos.x, 0.0, targetPos.z))
