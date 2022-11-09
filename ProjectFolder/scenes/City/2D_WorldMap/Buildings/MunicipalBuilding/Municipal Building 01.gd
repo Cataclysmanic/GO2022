9f12f6dd0994c52f4234c7ab148e43e92ff1bb4f
plexsoup <plexsoup@gmail.com>
2022-11-08 20:42:45 -0800

    can build static walls and light occluders from map image now.
    I'm not sure why the fudge factor of 3 keeps coming up, but whatever. Works for now.

--- ProjectFolder/scenes/City/2D_WorldMap/Buildings/MunicipalBuilding/Municipal Building 01.gd
+++ ProjectFolder/scenes/City/2D_WorldMap/Buildings/MunicipalBuilding/Municipal Building 01.gd
@@ -11,7 +11,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	update()
+	pass
+	#update()
 #	if ticks % 100 == 0:
 #		update()
 #	ticks += 1
@@ -20,24 +21,28 @@ func generate_light_occluders_from_bitmap():
 	var wallSprite = $Walls
 	var wallTex :Texture = wallSprite.get_texture()
 	var wallImage : Image = wallTex.get_data()
-	var rectSize = wallImage.get_size() * 3.0 * scale.x
-	var rectPos = get_global_position() - (rectSize/2)
+	var Stupid_Voodoo_Fudge_Factor = 3.0
+	var rectSize = wallImage.get_size() * scale.x * Stupid_Voodoo_Fudge_Factor
+	var rectPos = get_position()-(rectSize/2)
 	var wallRect = Rect2(rectPos, rectSize)
 	var wallBitmap = BitMap.new()
 	var xForm = get_transform() # what do I do with this???
 	wallBitmap.create_from_image_alpha(wallImage)
 	
 	occlusionPolygons = wallBitmap.opaque_to_polygons(wallRect)
-	var offsetPolygons = []
+#	var offsetPolygons = []
+#	for polygon in occlusionPolygons:
+#		var offsetPoly = translate_polygon(polygon, position)
+#		offsetPolygons.push_back(offsetPoly)
+	
+	
 	for polygon in occlusionPolygons:
-		var offsetPoly = translate_polygon(polygon, position/3)
-		offsetPolygons.push_back(offsetPoly)
-	
-	
-	for polygon in offsetPolygons:
-		var offsetPolygon = []
 		spawn_light_occluder(polygon)
 		spawn_static_body(polygon)
+		
+	$StaticBodyWalls.position = position/3 # I don't know where the 3 came from
+	$LightOccluders.position = position/3
+	
 
 func translate_polygon(polygon, translation : Vector2):
 	#I'm sure there's a number of built-in functions to do this with xForms, but I'm not sure how yet.
@@ -54,23 +59,32 @@ func spawn_light_occluder(polygon):
 	newOccluder.occluder = newOccPoly
 	$LightOccluders.add_child(newOccluder)
 
-func spawn_static_body(_polygon):
-	pass
+func spawn_static_body(myPolygon):
+	var newStatic = StaticBody2D.new()
+	var collisionPolygon = CollisionPolygon2D.new()
+	collisionPolygon.set_polygon(myPolygon)
+	newStatic.add_child(collisionPolygon)
+	$StaticBodyWalls.add_child(newStatic)
 	
-
+	var newPolyDraw = Polygon2D.new()
+	newPolyDraw.set_polygon(myPolygon)
+	newPolyDraw.set_color(Color.violet)
+	$StaticBodyWalls.add_child(newPolyDraw)
+	
 	
 func _draw():
 	
 	for polygon in occlusionPolygons:
-		var offsetPoly = []
-		for point in polygon:
-			offsetPoly.push_back(point + position/3) # How did you get 3? Show your work.
+#		var offsetPoly = []
+#		for point in polygon:
+#			offsetPoly.push_back(point + position/3) # How did you get 3? Show your work.
 #		var colors : PoolColorArray = []
 #		for _i in range(len(polygon)):
 #			colors.push_back(Color.blueviolet)
 #		draw_polygon(polygon, colors)
 
-		draw_colored_polygon(offsetPoly, Color.goldenrod)
+		#draw_colored_polygon(offsetPoly, Color.goldenrod)
+		pass
 		#draw_colored_polygon(polygon, Color.crimson)
 
 
