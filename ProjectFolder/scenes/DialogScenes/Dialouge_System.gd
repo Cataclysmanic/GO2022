d006de9830d5e82503bf929ec5b1c27d9cd549f9
NuttyNutHead <91644822+NuttyNutHead@users.noreply.github.com>
2022-11-10 16:39:06 -0500

    Added A Brand New Dialouge System that's much easier to use!
    You can add dialogue through the export variable, and add a voice there too.

---
+++ ProjectFolder/scenes/DialogScenes/Dialouge_System.gd
@@ -0,0 +1,36 @@
+extends CanvasLayer
+
+export(Array, String) var dialouge_Text = []
+export(AudioStream) var talking_audio
+
+var text_amount
+var current_text
+
+
+func _ready():
+	$AudioStreamPlayer.stream = talking_audio
+	
+	if dialouge_Text.size() != 0:
+		text_amount = dialouge_Text.size()
+		current_text = 0
+		$Control/Label.text = dialouge_Text[0]
+		$AudioStreamPlayer.playing = true
+		
+
+	
+func _physics_process(delta):
+	if Input.is_action_just_pressed("click"):
+		if current_text < text_amount - 1:
+			current_text += 1
+			$Control/Label.text = dialouge_Text[current_text]
+			$AudioStreamPlayer.playing = true
+		else:
+			$Fade_Player.play("Fade_out")
+
+
+func _on_AudioStreamPlayer_finished():
+	$AudioStreamPlayer.playing = false
+
+func _on_Fade_Player_animation_finished(anim_name):
+	print("This is where the scene would change")
+	##I dont know how to change the scenes to you guys can do that
