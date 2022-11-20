669d6a67dfc5ab1bece70967da7babc11b76243a
plexsoup <plexsoup@gmail.com>
2022-11-20 15:42:24 -0800

    populated the office with furniture

--- ProjectFolder/scenes/City/2D_WorldMap/Objects/KickableObject.gd
+++ ProjectFolder/scenes/City/2D_WorldMap/Objects/KickableObject.gd
@@ -13,8 +13,9 @@ func _ready():
 
 func _on_Body_entered(body):
 	if State == States.READY and body.has_method("is_player") and body.is_player():
-		print("kicked")
 		$AnimatedSprite.play("kick")
 		State = States.KICKED
 		$Area2D/CollisionShape2D.call_deferred("set_disabled", true)
-		
+		if has_node("Light2D"):
+			$Light2D.energy = 0.0
+			
