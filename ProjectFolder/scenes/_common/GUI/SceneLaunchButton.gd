a55cc9d8c2d4f4e6b1712caad4b87a361a2adcba
Benjamin <benjaminadji@hotmail.com>
2022-11-28 18:10:50 +0100

    added icons in tutorial section of journal

--- ProjectFolder/scenes/_common/GUI/SceneLaunchButton.gd
+++ ProjectFolder/scenes/_common/GUI/SceneLaunchButton.gd
@@ -11,7 +11,6 @@ var main_scene = "res://scenes/MenuScenes/MainMenu.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.grab_focus()
 	pass # Replace with function body.
 
 func _unhandled_input(event):
