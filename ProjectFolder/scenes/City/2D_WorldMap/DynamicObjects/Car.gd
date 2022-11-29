a74442139225f33444cb3ede708b6cc08ee7dcf7
plexsoup <plexsoup@gmail.com>
2022-11-29 02:53:25 -0800

    started implementing a new cop npc
    They're still buggy. Don't track the player well and don't shoot at the player.
    
    Note, they're not in MookNPC, they have their own scene and script.

--- ProjectFolder/scenes/City/2D_WorldMap/DynamicObjects/Car.gd
+++ ProjectFolder/scenes/City/2D_WorldMap/DynamicObjects/Car.gd
@@ -16,7 +16,7 @@ var max_npcs = 5
 var npcs_spawned = 0
 var chance_to_spawn_NPCs = 0.1
 var npc_patrol_route # PathFollow object for pedestrian cops to walk around, once spawned
-
+var npc_to_spawn
 
 var city_map
 var home_building
@@ -33,8 +33,15 @@ func _ready():
 	State = States.READY
 	city_map = Global.world_controller.current_scene
 	home_building = city_map.get_random_building()
+	choose_npc_type()
 
 	start_random_engine()
+
+func choose_npc_type():
+	var npcResourceName = "CopNPC"
+	if randf() < 0.2:
+		npcResourceName = "MookNPC"
+	npc_to_spawn = $AvailableNPCs.get_resource(npcResourceName)
 
 func start_random_engine():	
 	var engines = $Audio/Engine.get_children()
@@ -241,9 +248,7 @@ func spawn_path_follower(routeScene):
 
 func spawn_npc():
 	# Generate cops that roam around on a simple path
-	var npcs = $AvailableNPCs.get_resource_list()
-	var randomNPCName = npcs[randi()%npcs.size()]
-	var npcScene = $AvailableNPCs.get_resource(randomNPCName).instance()
+	var npcScene = npc_to_spawn.instance()
 	npcScene.set_global_position($NPCSpawnPosition.get_global_position())
 	var pathFollower = spawn_path_follower(npc_patrol_route)
 	npcScene.init(city_map, home_building, pathFollower)
---
+++ ProjectFolder/scenes/NPCs/CopPaperDoll.gd
@@ -0,0 +1,73 @@
+extends Node2D
+
+var melee_damage = 15
+# var player
+
+
+func _ready():
+	pass
+	#player = get_parent()
+	
+func start_running():
+	$AnimationPlayer.play("Run")
+
+
+func start_walking():
+	$AnimationPlayer.play("Walk")
+	
+	
+func relax():
+	start_idling()
+	
+func start_idling():
+	$AnimationPlayer.play("Relax")
+	
+func point_gun():
+	$AnimationPlayer.play("PointGun")
+
+func melee_attack():
+	if randf() < 0.5:
+		$AnimationPlayer.play("melee attack 1")
+	elif randf() < 0.95:
+		$AnimationPlayer.play("melee attack 2")
+
+
+func dash():
+	$AnimationPlayer.play("dash")
+
+
+func get_animation():
+	var anim_queue = $AnimationPlayer.get_queue()
+	if len(anim_queue) > 0:
+		return anim_queue[0]
+
+func hit(): # receive damage
+	$AnimationPlayer.play("hit")
+
+func die():
+	$AnimationPlayer.play("die")
+
+func _process(_delta):
+	if Global.player != null and is_instance_valid(Global.player):
+		aim_toward(Global.player.global_position - global_position)
+	
+func aim_toward(dirVector):
+	# Just point the gun at the player.
+	# should be easy
+	
+	if dirVector.x > 0:
+		$Torso/Torso/GunArmHolder.rotation = - dirVector.angle()
+	else:
+		$Torso/Torso/GunArmHolder.rotation = PI + dirVector.angle()
+		
+	
+
+
+func _on_DamageArea_body_entered(body):
+	if body != Global.player and body.has_method("_on_hit"):
+		if body.has_method("extreme_knock_back"):
+			body.extreme_knock_back(self.global_position.direction_to(body.global_position))
+		#body._on_hit(melee_damage)
+		$Upper/Fist.hide()
+		var collisionShape = $Upper/Fist/DamageArea/CollisionShape2D
+		collisionShape.set_deferred("disabled", true)
